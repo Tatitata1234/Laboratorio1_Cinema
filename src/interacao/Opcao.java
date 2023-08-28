089c8d95f4be8ae187f83a706f307ae9f7327b70
Tatitata1234 <rhais.s.land@gmail.com>
2023-08-28 20:36:37 -0300

    base para menu e opcao

---
+++ src/interacao/Opcao.java
@@ -0,0 +1,33 @@
+package interacao;
+
+import cinema.Assento;
+
+//Escreva uma aplicação que apresente um menu de opções, permitindo fazer uma reserva, cancelar uma
+//        reserva, mostrar quais assentos estão livres/ocupados (mapa) e mostrar o total de lugares livres/ocupados.
+//        Usem sua criatividade para construir o software.
+//        Procure separar as classes com base nas suas funcionalidades, inclusive nos pacotes que acharem
+//        adequados.
+//        Sugestão de classes: Menu, Opcoes (reservar, cancelar, mostrar mapa e quantidades), Mapa e Assento.
+//        Dica: Para entrada de dados, sugiro usar a Classe Scanner, da API do Java.
+public class Opcao {
+
+    public boolean reservar(Assento assento){
+
+    }
+
+    public boolean cancelar(Assento assento){
+
+    }
+
+    public void mostrarMapa(){
+
+    }
+
+    public int calcularQuantidadeAssentosLivres(Mapa mapa){
+
+    }
+
+    public int calcularQuantidadeAssentosOcupados(Mapa mapa){
+
+    }
+}
